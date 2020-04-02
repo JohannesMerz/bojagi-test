c55884a0d96d8b9c77ddfe6dd2ee58418230e6b3
JohannesMerz <johannes_merz@gmx.de>
2020-04-02 21:53:34 +0200

    rm a story

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styled from 'styled-components'
 import Box from './components/Box'
 import Button from './components/Button'
