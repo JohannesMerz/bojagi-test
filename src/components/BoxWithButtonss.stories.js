5ec9bea38f5ab6268609a864ef5fdc5327645501
JohannesMerz <johannes_merz@gmx.de>
2021-04-13 18:36:07 +0200

    rename component and story

--- src/components/BoxWithButtonss.stories.js
+++ src/components/BoxWithButtonss.stories.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import BoxWithButtons from './BoxWithButtons'
+import BoxWithButtons from './BoxWithButtonss'
 
-export default { title: 'BoxWithButtons' };
+export default { title: 'BoxWithButtonss' };
 
 export const trafficLight = () => <BoxWithButtons color="white" buttons={[{text: 'stop', color: 'red'},{text: 'ready', color: 'yellow'}, {text: 'go', color: 'green'}]}/>
