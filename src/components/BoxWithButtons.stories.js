ef8c72e9cc2022f417fe383c0bc9989eb265acf3
JohannesMerz <johannes_merz@gmx.de>
2021-04-16 16:26:45 +0200

    2021-04-16-002

--- src/components/BoxWithButtons.stories.js
+++ src/components/BoxWithButtons.stories.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import BoxWithButtons from './BoxWithButtonss'
+import BoxWithButtons from './BoxWithButtons'
 
-export default { title: 'BoxWithButtonss' };
+export default { title: 'BoxWithButtons' };
 
 export const trafficLight = () => <BoxWithButtons color="white" buttons={[{text: 'stop', color: 'red'},{text: 'ready', color: 'yellow'}, {text: 'go', color: 'green'}]}/>
